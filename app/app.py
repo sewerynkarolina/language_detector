b363e8135e143b278fb3204acd73025ce900d84c
Karolina <sewerynnkarolinaa@gmail.com>
2019-05-22 16:59:39 +0200

    wczytywanie pdfa ze sciezki

--- app/app.py
+++ app/app.py
@@ -3,7 +3,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from io import BytesIO
-from tika import parser
+import prepare_data as prep_data
+import base64
+import io
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
         'https://github.com/plotly/dash-salesforce-crm/blob/master/static/s8.css',
         'https://github.com/plotly/dash-salesforce-crm/blob/master/static/s4.css']
@@ -36,29 +39,58 @@ app.layout = html.Div([
         multiple=False
     )),
 
+    html.H4('Enter a path to pdf file'),
+    html.Div(dcc.Input(
+        id='data_path',
+        placeholder='Enter a path to pdf file',
+        type='text',
+        value=''
+    )),
+
+    html.Div(id='data_output'),
+
     html.Div(id='output-data-upload')
 ])
 
 
+@app.callback(Output('data_output', 'children'),
+              [Input('data_path', 'value')])
+def update_output(data_path):
+    return prep_data.convert_pdf_to_txt(data_path)
+
 
 @app.callback(
 Output('output-data-upload', 'children'),
-[Input('upload-data', 'contents')])
+[Input('upload-data', 'contents')])#,
+#[State('upload-data', 'filename'),
+#State('upload-data', 'last_modified')])
 def update(content):
     pass
-    # print(type(content))
-    # print(type(str.encode(content)))
-    # # creating a pdf reader object
-    # pdfReader = parser.from_buffer(BytesIO(str.encode(content)))
-    # print(pdfReader['content'])
-    # # printing number of pages in pdf file
-    # print(pdfReader.numPages)
-    # # creating a page object
-    # pageObj = pdfReader.getPage(0)
-    # # extracting text from page
-    # print(pageObj.extractText())
-    # # closing the pdf file object
-    # pdfFileObj.close()
+    # if content is not None:
+    #     content_type, content_string = content.split(',')
+    #     decoded = base64.b64decode(content_string)
+    #     print(decoded)
+    #     print(io.BytesIO(decoded))
+        #d = prep_data.convert_pdf_to_txt(io.BytesIO(decoded))
+        #children = [
+    #        parse_contents(c, n, d) for c, n, d in
+    #        zip(list_of_contents, list_of_names, list_of_dates)]
+    #children
+    # if content is not None:
+    #     #print(content)
+    #     print(type(content))
+    #     print(type(str.encode(content)))
+    #     print(prep_data.convert_pdf_to_txt(content))
+
+    #pass
+    #print(type(content))
+    #print(type(str.encode(content)))
+    # creating a pdf reader object
+    #pdf = pdftotext.PDF((BytesIO(str.encode(content))))
+    #el_of_list = ''
+    #Ponieważ page in pdf  - to jest strona z artykułu to łącze stringi, pewnie to można lepiej
+    #for page in pdf:
+    #    el_of_list = el_of_list+page
 
 if __name__ == '__main__':
     app.run_server(debug=True)
