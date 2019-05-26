f1cd79bea77f388dd8014400cc357fab26afa614
lorenckat <32520444+lorenckat@users.noreply.github.com>
2019-05-26 10:56:54 +0200

    explainer w nowym oknie, wyglad

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from dash.dependencies import Input, Output
 from io import BytesIO
 import prepare_data as prepare_data
 import base64
+import webbrowser
 import io
 import json
 from sklearn.externals import joblib
@@ -14,9 +15,10 @@ from lime.lime_text import LimeTextExplainer
 from sklearn.pipeline import make_pipeline
 import dash_dangerously_set_inner_html
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
-#         'https://github.com/plotly/dash-salesforce-crm/blob/master/static/s8.css',
-#         'https://github.com/plotly/dash-salesforce-crm/blob/master/static/s4.css']
+external_css = ["https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
+            "//fonts.googleapis.com/css?family=Raleway:400,300,600",
+            "//fonts.googleapis.com/css?family=Dosis:Medium",
+            "https://cdn.rawgit.com/plotly/dash-app-stylesheets/0e463810ed36927caf20372b6411690692f94819/dash-drug-discovery-demo-stylesheet.css"]
 
 app = dash.Dash(__name__)#, #external_stylesheets=external_stylesheets)
 
@@ -30,35 +32,47 @@ explainer = LimeTextExplainer()
 with open("../columns.json", "r") as f:
     column_names = json.load(f)
 
-app.layout = html.Div([
-        html.H1('Language detector'),
+def layout_app(app):
+    app.layout = html.Div([
+       
 
-    html.Div('''
-        by: K. Banecki, K. Lorenc, J. Piega, M.Seliga, K.Seweryn
-    '''),
+                    html.H1('Language detector'),
 
-    html.Div(dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            html.Button('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        multiple=False
-    )),
-    html.Div(id="explain-frame"),
-    html.Div(id='output-data-upload',  style={"height": "600px", "overflow-y": "scroll"}),
-])
+                    html.Div('''
+                        by: K. Banecki, K. Lorenc, J. Piega, M.Seliga, K.Seweryn
+                    '''),
+
+                    html.Div(dcc.Upload(
+                        id='upload-data',
+                        children=html.Div([
+                            html.Button('Select Files')
+                        ]),
+                        style={
+                            'width': '100%',
+                            'height': '60px',
+                            'lineHeight': '60px',
+                            'borderWidth': '1px',
+                            'borderStyle': 'dashed',
+                            'borderRadius': '5px',
+                            'textAlign': 'center',
+                            'margin': '10px'
+                        },
+                        multiple=False
+                    )),
+                    html.Div(id='display-selected-values'),
+                    html.Div(id="explain-frame"),
+                    html.Div(id='output-data-upload',  style={"height": "600px", "overflow-y": "scroll"}),
 
 
+
+    ])
+    for css in external_css:
+        app.css.append_css({"external_url": css})
+
+    return (app)
+
+
+app = layout_app(app)
 
 
 @app.callback(
@@ -78,6 +92,8 @@ def make_prediction(contents):
     X = prepare_data.prepare_to_model(contents[0], column_names, pages)
     exp = explainer.explain_instance(X, pp.predict_proba, num_features=6)
     exp.save_to_file("explain.html")
+    url = "explain.html"
+    webbrowser.open(url,new=1)
     return dash_dangerously_set_inner_html.DangerouslySetInnerHTML(exp.as_html())
 
 
